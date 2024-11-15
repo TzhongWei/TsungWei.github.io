6042a3f9802ce8a27445ddebaa47543bc146ba65
TsungWei Cheng <mike861104@gmail.com>
2024-11-15 10:28:22 +0800

    ResumeLink

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -83,7 +83,7 @@ function Header() {
           <li>
             <a
               href={
-                "https://drive.google.com/file/d/1Czlus4EA-J6bmF3lz3dB1dwQdhmkxVXN/view?usp=drive_link"
+                "https://drive.google.com/file/d/19rpBwlOV-Bdg-v1BKIxcyrQWgXug4SRU/view?usp=drive_link"
               }
               target="_blank"
               rel="noreferrer noopener"
